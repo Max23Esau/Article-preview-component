4541da0d3fa13fd849c558b440d2bb9c8081a8c6
Max23Esau <esa-max23@live.com>
2020-10-16 21:31:55 -0500

    Challenge completed

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-if (document.getElementById('btnModal')) {
-  var modalContainer = document.getElementById('ModalContainer');
-  var btn = document.getElementById('btnModal');
-  var body = document.getElementsByTagName('body')[0];
+var modalContainer = document.getElementById('ModalContainer');
+var btn = document.getElementById('btnModal');
+var share = document.getElementById('userDescription');
+var span = document.getElementsByClassName('close')[0];
 
-  btn.onclick = function () {
-    modalContainer.style.display = 'block';
-  };
+if (window.matchMedia('(min-width: 890px)').matches) {
+  /* La pantalla tiene al menos 890 píxeles de ancho */
+  if (document.getElementById('btnModal')) {
+    btn.onclick = function () {
+      modalContainer.style.display = 'grid';
+    };
 
-  window.onclick = function (event) {
-    if (event.target == modalContainer) {
+    span.onclick = function () {
       modalContainer.style.display = 'none';
-    }
-  };
+    };
+  }
+} else {
+  /* La pantalla tiene menos de 890 píxeles de ancho */
+  if (document.getElementById('btnModal')) {
+    btn.onclick = function () {
+      modalContainer.style.display = 'grid';
+      share.classList.add('hide');
+      modalContainer.classList.add('active');
+    };
+
+    span.onclick = function () {
+      modalContainer.style.display = 'none';
+      share.classList.remove('hide');
+    };
+  }
 }
