3f6d3265a2e3d129fb80fe8a0af2a53f0a409744
Max23Esau <esa-max23@live.com>
2020-10-19 17:52:28 -0500

    fixed the modal

--- index.js
+++ index.js
@@ -4,45 +4,61 @@ var btnContainer = document.getElementById('userInfo__button');
 var share = document.getElementById('userDescription');
 var span = document.getElementsByClassName('close')[0];
 
-if (window.matchMedia('(min-width: 890px)').matches) {
-  /* La pantalla tiene al menos 890 píxeles de ancho */
-  if (document.getElementById('btnModal')) {
-    btn.onclick = function () {
-      modalContainer.style.display = 'grid';
-    };
+// if (window.matchMedia('(min-width: 890px)').matches) {
+//   /* La pantalla tiene mas de 890 píxeles de ancho */
+//   if (document.getElementById('btnModal')) {
+//     btn.onclick = function () {
+//       modalContainer.style.display = 'grid';
+//     };
 
-    span.onclick = function () {
-      modalContainer.style.display = 'none';
-    };
-  }
-} else {
-  /* La pantalla tiene menos de 890 píxeles de ancho */
-  if (document.getElementById('btnModal')) {
-    btn.onclick = function () {
-      modalContainer.style.display = 'grid';
-      share.classList.add('hide');
-      modalContainer.classList.add('active');
-    };
+//     span.onclick = function () {
+//       modalContainer.style.display = 'none';
+//     };
+//   }
+// } else {
+//   /* La pantalla tiene menos de 890 píxeles de ancho */
+//   if (document.getElementById('btnModal')) {
+//     btn.onclick = function () {
+//       modalContainer.style.display = 'grid';
+//       share.classList.add('hide');
+//       btnContainer.classList.add('hide');
+//       // modalContainer.classList.add('active');
+//     };
 
-    span.onclick = function () {
-      modalContainer.style.display = 'none';
-      share.classList.remove('hide');
-    };
-  }
-}
+//     span.onclick = function () {
+//       modalContainer.style.display = 'none';
+//       share.classList.remove('hide');
+//       btnContainer.classList.remove('hide');
+//     };
+//   }
+// }
 
-if (window.matchMedia('(min-width: 449px)').matches) {
-  if (document.getElementById('btnModal')) {
-    btn.onclick = function () {
-      modalContainer.style.display = 'grid';
-      share.classList.add('hide');
-      btnContainer.classList.add('hide');
+// if (window.matchMedia('(min-width: 449px)').matches) {
+//   if (document.getElementById('btnModal')) {
+//     btn.onclick = function () {
+//       modalContainer.style.display = 'grid';
+//       share.classList.add('hide');
+//       btnContainer.classList.add('hide');
 
-      span.onclick = function () {
-        modalContainer.style.display = 'none';
-        share.classList.remove('hide');
-        btnContainer.classList.remove('hide');
-      };
-    };
+//       span.onclick = function () {
+//         modalContainer.style.display = 'none';
+//         share.classList.remove('hide');
+//         btnContainer.classList.remove('hide');
+//       };
+//     };
+//   };
+// };
+
+if (document.getElementById('btnModal')) {
+  btn.onclick = function () {
+    modalContainer.style.display = 'grid';
+    share.classList.add('hide');
+    btnContainer.classList.add('hide');
   };
-};
+
+  span.onclick = function () {
+    modalContainer.style.display = 'none';
+    share.classList.remove('hide');
+    btnContainer.classList.remove('hide');
+  };
+}
